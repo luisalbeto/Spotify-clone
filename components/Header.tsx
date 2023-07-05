241b6676556c7302520de2e6180e721c2c2bcdab
luisalbeto <luis-alberto11@hotmail.com>
2023-07-05 12:48:15 -0400

    Finishing Layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,16 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi"
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
-  classname: string;
+  className: string;
 }
 
 const Header: React.FC<HeaderProps> = ({
   children,
-  classname
+  className
 }) => {
   const router = useRouter();
 
@@ -27,7 +28,7 @@ const Header: React.FC<HeaderProps> = ({
       bg-gradient-to-b
       from-emerald-800
       p-6`,
-      classname
+      className
       )}
       >
         <div className="
@@ -91,9 +92,41 @@ const Header: React.FC<HeaderProps> = ({
                 transition">
                 <BiSearch className="text-black" size={20}/>
               </button>
+            </div>
+            <div className="
+              flex
+              justify-between
+              items-center
+              gap-x-4
+              ">
+                <>
+                <div>
+                <Button
+                    onClick={() => {}}
+                    className="
+                      bg-transparent
+                      text-neutral-300
+                      font-medium"> 
+                    Sing Up
+                  </Button>
+                </div>
+
+                <div>
+                <Button
+                    onClick={() => {}}
+                    className="
+                      bg-white
+                      px-6
+                      py-2"> 
+                    Log in
+                  </Button>
+                </div>
+                  
+                </>
 
             </div>
         </div>
+        {children}
     </div>
   );
 }
